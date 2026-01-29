39245b694bcc9d6b0de236871ba9ac73efad44e4
gonzalez-7 <20agonzalezp@gmail.com>
2026-01-29 13:27:26 -0800

    Added the tittle, time based age calculation and the readiness category logic.

--- COMP003A.Assignment3/Program.cs
+++ COMP003A.Assignment3/Program.cs
@@ -1,10 +1,44 @@
-﻿namespace COMP003A.Assignment3
+﻿using System.Data;
+
+namespace COMP003A.Assignment3
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("=== Academic & Personal Readiness Evaluator ===");
+            Console.WriteLine();
+
+            //Behavior 1
+
+            int currentYear = DateTime.Now.Year;
+            Console.WriteLine("Current Year: " + currentYear);
+
+            Console.WriteLine("Enter your birth year: ");
+            string birthYearInput = Console.ReadLine();
+            int birthYear = int.Parse(birthYearInput);
+
+            int age = currentYear - birthYear;
+            Console.WriteLine();
+            Console.WriteLine("Calculated Age: " + age);
+
+            string readinessCategory;
+
+            if (age < 18)
+            {
+                readinessCategory = "Minor";
+            }
+            else if (age <= 64)
+            {
+                readinessCategory = "Adult";
+            }
+            else
+            {
+                readinessCategory = "Senior";
+            }
+
+            Console.WriteLine("Readiness Category: " + readinessCategory);
+            Console.WriteLine();
         }
     }
 }
